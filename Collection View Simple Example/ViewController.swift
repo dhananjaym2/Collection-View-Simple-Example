8362377574b991715283ef3df4d5cb862e75ca72
dhananjay.mohnot <dhananjay.mohnot@zinier.com>
2021-04-02 18:41:21 +0530

    Initial Commit

---
+++ Collection View Simple Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection View Simple Example
+//
+//  Created by Dhananjay.Mohnot on 02/04/21.
+//  Copyright © 2021 Dhananjay.Mohnot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
