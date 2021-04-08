2297cf1febdc7c6c76943cc5662f4edf37338f42
dhananjaym2 <dhananjaymohnot@gmail.com>
2021-04-08 06:35:19 +0530

    Simple collection view with the cells having numbers with vertical scroll.

---
+++ Collection View Simple Example/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  Collection View Simple Example
+//
+//  Created by Dhananjay.Mohnot on 02/04/21.
+//  Copyright © 2021 Dhananjay.Mohnot. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+}
